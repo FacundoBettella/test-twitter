ddffea740413a4750bd071f6d445346f0a3c4dcf
Facundo <facundobettella@outlook.com>
2021-07-29 01:45:01 -0300

    front test

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import dotenv from "dotenv";
+import axios from 'axios';
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <React.StrictMode>
