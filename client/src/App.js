22f1d06e7a2926c9663e7e3e7d4d96216dbbf8d3
Facundo <facundobettella@outlook.com>
2021-07-29 00:51:21 -0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Example from './Example';
+import Twitter from './Twitter';
+
+function App() {
+  return (
+    <div className="App">
+        <Example />
+        <Twitter />
+    </div>
+  );
+}
+export default App;
